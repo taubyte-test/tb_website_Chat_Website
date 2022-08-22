5f53c12d8d4a0c46b9106c0e5eb4394d8c3b908c
Aron Jalbuena <aron@taubyte.com>
2022-08-22 14:07:09 -0500

    shortened script

--- taubyte/build.sh
+++ taubyte/build.sh
@@ -1,29 +1,18 @@
 # !/bin/bash
 
-echo "here 1"
-
 set +x
 
-echo "here 2"
-
 exec 2>&1
-
-echo "here 3"
 
 (
     (which npx) || npm install -g npx
 
     test -d out || mkdir -p "out"
-    rm -fr out/*
-) || true
 
-echo "here 4"
+    rm -fr out/*
+)
 
 cp -r backend frontend/* out
-
-echo "here 5"
-
-set -a pids
 
 cd out
 for name in $(find . -iname "*.html" -o -iname "*.js" -o -iname "*.css")
