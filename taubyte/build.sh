6e231af3fc13e2b4a4ce8a4e49b2b33d9206a43f
Aron Jalbuena <aron@taubyte.com>
2022-08-22 13:25:41 -0500

    script wip

---
+++ taubyte/build.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+
+(which npx) || npm install -g npx
+
+cp -r backend frontend/* out
+
+set -a pids
+
+cd out
+for name in $(find . -iname "*.html" -o -iname "*.js" -o -iname "*.css")
+do
+   (
+    echo $name
+    npx minify ${name} > ${name}.min
+    rm ${name}
+    mv ${name}{.min,}
+   ) & 
+   pids+=( $! )
+done
+
+# wait for all pids
+for pid in ${pids[@]}; do
+    wait $pid
+done
