7dd9381e6d7e8d96f15afa2b34884741c9ce123d
taubyte-test <104384673+taubyte-test@users.noreply.github.com>
2022-09-12 12:09:15 -0500

    Update build.sh

--- taubyte/build.sh
+++ taubyte/build.sh
@@ -7,12 +7,12 @@ exec 2>&1
 (
     (which npx) || npm install -g npx
 
-    rm -fr out/*
+    rm -fr /out/*
 )
 
-cp -r backend frontend/* out
+cp -r backend frontend/* /out
 
-cd out
+cd /out
 for name in $(find . -iname "*.html" -o -iname "*.js" -o -iname "*.css")
 do
     npx minify ${name} > ${name}.min
